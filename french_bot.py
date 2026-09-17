6a095cb1e140dd0cf12588bacb5a78d6c0ff8f4b
tahaqureshicongo-dev <taha.qureshi.congo@gmail.com>
2026-09-17 23:24:03 +0300

    Update french_bot.py

--- french_bot.py
+++ french_bot.py
@@ -1,3 +1,19 @@
+import os
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import threading
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is alive!")
+
+def run_server():
+    port = int(os.environ.get("PORT", 8080))
+    server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_server, daemon=True).start()
 import asyncio
 import schedule
 import time
